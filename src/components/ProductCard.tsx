27a6df8cece78ad445a47f59d2626e8f9c114bd0
Danie3L <danielzukowski5@gmail.com>
2024-01-23 20:27:54 +0100

    feat: added logic for displaying products list

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,20 @@
-export default function ProductCard() {
+type ProductCardProps = {
+  title: string;
+  price: number;
+  imgSrc: string;
+  id?: number;
+};
+
+export default function ProductCard({
+  title,
+  price,
+  imgSrc,
+}: ProductCardProps) {
   return (
     <section>
-      <h2>Widget</h2>
-      <img src="" alt="" />
-      <span>$9.99</span>
+      <h2>{title}</h2>
+      <img src={imgSrc} alt={title} />
+      <span>${price}</span>
       <button>Add to Cart</button>
     </section>
   );
--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -1,8 +1,19 @@
+import ProductCard from "./ProductCard";
+import { productsData } from "../data";
 export default function ProductsSection() {
   return (
     <main>
       <article>
-        <ul></ul>
+        <h1>hi</h1>
+        <ul>
+          {productsData.map((product) => {
+            return (
+              <li>
+                <ProductCard key={product.id} {...product} />
+              </li>
+            );
+          })}
+        </ul>
       </article>
     </main>
   );
