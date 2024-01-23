d7dae730701a6b856bf6ad01c2b68a3a999b96fc
Danie3L <danielzukowski5@gmail.com>
2024-01-23 18:40:47 +0100

    feat: add Header component with jsx structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Acme Co.</h1>
+      <div>
+        <p>
+          Total items: <span>0</span>
+        </p>
+        <p>
+          Total price: <span>$0.00</span>
+        </p>
+        <button>View Cart</button>
+      </div>
+    </header>
+  );
+}
