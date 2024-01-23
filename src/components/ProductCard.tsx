d018d9aae3f92a6b2a8a1044c2d0602c9f13789e
Danie3L <danielzukowski5@gmail.com>
2024-01-23 20:02:20 +0100

    refactor: changed Product.tsx component name to ProductCard.tsx

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+export default function ProductCard() {
+  return (
+    <section>
+      <h2>Widget</h2>
+      <img src="" alt="" />
+      <span>$9.99</span>
+      <button>Add to Cart</button>
+    </section>
+  );
+}
