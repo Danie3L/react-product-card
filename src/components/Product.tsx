568a58f486bcb8d4db70f0a0651a0d5ff9418566
Danie3L <danielzukowski5@gmail.com>
2024-01-23 19:10:42 +0100

    feat: added ProductsSection and Product components and their basic jsx markup

---
+++ src/components/Product.tsx
@@ -0,0 +1,10 @@
+export default function Product() {
+  return (
+    <section>
+      <h2>Widget</h2>
+      <img src="" alt="" />
+      <span>$9.99</span>
+      <button>Add to Cart</button>
+    </section>
+  );
+}
