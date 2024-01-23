5948f67c7c7fef86663eeeb6129c2f34c47334b8
Danie3L <danielzukowski5@gmail.com>
2024-01-23 19:59:48 +0100

    refactor: added data.ts file which stores data for every single product

---
+++ src/data.ts
@@ -0,0 +1,31 @@
+import widget from "./assets/widget-1.jpg";
+import premiumWidget from "./assets/widget-2.jpg";
+import deluxeWidget from "./assets/widget-3.jpg";
+
+type productsDataProps = {
+  title: string;
+  price: number;
+  imgSrc: string;
+  id: number;
+};
+
+export const productsData: productsDataProps[] = [
+  {
+    title: "Widget",
+    price: 9.99,
+    imgSrc: "/src/assets/widget-1.jpg",
+    id: 0,
+  },
+  {
+    title: "Premium Widget",
+    price: 19.99,
+    imgSrc: "/src/assets/widget-2.jpg",
+    id: 1,
+  },
+  {
+    title: "Deluxe Widget",
+    price: 29.99,
+    imgSrc: "/src/assets/widget-3.jpg",
+    id: 2,
+  },
+];
